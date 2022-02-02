d9c17d184a548f10d1ab3663e040ab66635e7a4f
Kosuke2000 <youuchiharu@gmail.com>
2022-02-02 15:16:12 +0900

    NoteStyleのレスポンシブ作り込み

--- src/components/model/LinkCard/NoteStyle/View.tsx
+++ src/components/model/LinkCard/NoteStyle/View.tsx
@@ -11,11 +11,11 @@ export const LinkCardView: VFC<LinkCardViewProps> = ({ ogp }) => {
   const w = ogImgUrl ? "w-full md:w-3/5" : "w-full";
 
   return (
-    <article className="flex justify-between w-full max-w-2xl h-40 border border-gray-200 border-solid">
+    <article className="flex justify-between w-full max-w-2xl h-32 border border-gray-200 border-solid md:h-40">
       <div
         className={`flex overflow-hidden flex-col justify-around md:justify-between p-8 px-5 hover:bg-gray-100 ${w}`}
       >
-        <h1 className="overflow-hidden max-h-12 text-xl font-medium leading-6 text-ellipsis">
+        <h1 className="overflow-hidden max-h-12 text-xl font-medium leading-5 text-ellipsis md:leading-6">
           {title}
         </h1>
         <p className="hidden text-sm text-gray-400 truncate md:block">
@@ -29,7 +29,7 @@ export const LinkCardView: VFC<LinkCardViewProps> = ({ ogp }) => {
       <div className="min-w-max md:w-2/5">
         <img
           src={ogImgUrl}
-          className="object-cover w-40 h-40 md:w-full"
+          className="object-cover w-32 h-32 md:w-full md:h-40"
           alt=""
         />
       </div>
