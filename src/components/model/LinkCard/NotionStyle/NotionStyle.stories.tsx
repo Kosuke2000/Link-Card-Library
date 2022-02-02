b48a5ae52539af72d88820a7d47d1c36e7d5d180
Kosuke2000 <youuchiharu@gmail.com>
2022-02-02 16:28:28 +0900

    NotionStyle見た目の作り込み

--- src/components/model/LinkCard/NotionStyle/NotionStyle.stories.tsx
+++ src/components/model/LinkCard/NotionStyle/NotionStyle.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import React from "react";
 
-import { mockOgpData2 } from "@/mocks/LinkCard";
+import { mockOgpData, mockOgpData2 } from "@/mocks/LinkCard";
 
 import { LinkCardView } from "./View";
 
@@ -13,35 +13,16 @@ export default {
   },
 } as ComponentMeta<typeof LinkCardView>;
 
-const Small: ComponentStory<typeof LinkCardView> = (args) => (
-  <div className="max-w-sm border-2">
-    <LinkCardView {...args} />
-  </div>
+const Template: ComponentStory<typeof LinkCardView> = (args) => (
+  <LinkCardView {...args} />
 );
 
-const Medium: ComponentStory<typeof LinkCardView> = (args) => (
-  <div className="max-w-md border-2">
-    <LinkCardView {...args} />
-  </div>
-);
-
-const Large: ComponentStory<typeof LinkCardView> = (args) => (
-  <div className="max-w-6xl border-2">
-    <LinkCardView {...args} />
-  </div>
-);
-
-export const Mobile = Small.bind({});
-Mobile.args = {
-  ogp: mockOgpData2,
+export const Default = Template.bind({});
+Default.args = {
+  ogp: mockOgpData,
 };
 
-export const Tablet = Medium.bind({});
-Tablet.args = {
-  ogp: mockOgpData2,
-};
-
-export const Desktop = Large.bind({});
-Desktop.args = {
+export const LongDescription = Template.bind({});
+LongDescription.args = {
   ogp: mockOgpData2,
 };
--- src/components/model/LinkCard/NotionStyle/View.tsx
+++ src/components/model/LinkCard/NotionStyle/View.tsx
@@ -10,22 +10,35 @@ export const LinkCardView: VFC<LinkCardViewProps> = ({ ogp }) => {
   const ml = faviconUrl ? "ml-2" : "";
 
   return (
-    <article className="flex justify-between w-full max-w-3xl h-40 rounded border border-gray-400 border-solid">
-      <div
-        className={`flex flex-col justify-between p-5  hover:bg-gray-100 ${w}`}
+    <article className="flex justify-between w-full max-w-3xl h-32 rounded border border-gray-400 border-solid">
+      {/* <div
+        className="flex overflow-hidden flex-col justify-between p-5 py-4"
+        style={{ width: `calc(100%-256px)` }}
       >
-        <h3 className="text-2xl truncate">{title}</h3>
-        <p className="overflow-hidden h-12 text-base text-gray-500">
+        <h3 className="text-lg truncate">{title}</h3>
+        <p className="overflow-hidden h-8 text-sm leading-4 text-gray-500">
           {description}
         </p>
         <div className="flex items-center">
-          {faviconUrl && <img src={faviconUrl} className="h-6" alt="" />}
-          <p className={`text-base truncate ${ml}`}>{pageUrl}</p>
+          {faviconUrl && <img src={faviconUrl} className="h-5" alt="" />}
+          <p className={`text-sm truncate ${ml}`}>{pageUrl}</p>
+        </div>
+      </div> */}
+      <div
+        className={`flex overflow-hidden flex-col justify-between p-5 py-4 ${w}`}
+      >
+        <h3 className="text-lg truncate">{title}</h3>
+        <p className="overflow-hidden h-8 text-sm leading-4 text-gray-500">
+          {description}
+        </p>
+        <div className="flex items-center">
+          {faviconUrl && <img src={faviconUrl} className="h-5" alt="" />}
+          <p className={`text-sm truncate ${ml}`}>{pageUrl}</p>
         </div>
       </div>
       {ogImgUrl && (
-        <div className="hidden w-2/5 h-full rounded md:block">
-          <img src={ogImgUrl} className="object-cover w-full h-full" alt="" />
+        <div className="hidden sm:w-2/5 md:block">
+          <img src={ogImgUrl} className="object-cover w-full h-32" alt="" />
         </div>
       )}
     </article>
