6c8b2faed12b1e49a1f094d0c271eec8e8da99ed
Kosuke2000 <youuchiharu@gmail.com>
2022-02-03 09:09:08 +0900

    TwitterStyleの微調整

--- src/components/model/LinkCard/TwitterStyle/View.tsx
+++ src/components/model/LinkCard/TwitterStyle/View.tsx
@@ -9,16 +9,16 @@ export const LinkCardView: VFC<LinkCardViewProps> = ({ ogp }) => {
   const { title, description, pageUrl, ogImgUrl } = ogp;
 
   return (
-    <article className="w-full max-w-lg rounded-xl border border-slate-300 border-solid">
+    <article className="w-full max-w-lg rounded-xl border border-gray-300 border-solid">
       <img
         src={ogImgUrl}
         className="object-cover w-full h-48 rounded-t-xl md:h-60"
         alt=""
       />
-      <div className="overflow-hidden p-3 w-full max-h-32 hover:bg-gray-100">
-        <p className="text-xs text-gray-500">{urlToDomain(pageUrl)}</p>
-        <h2 className=" truncate">{title}</h2>
-        <p className="overflow-hidden max-h-10 text-base leading-5 text-gray-500 ">
+      <div className="flex overflow-hidden flex-col justify-around p-3 w-full max-h-32 hover:bg-gray-100">
+        <p className="text-sm text-gray-600">{urlToDomain(pageUrl)}</p>
+        <h2 className="font-semibold truncate">{title}</h2>
+        <p className="overflow-hidden max-h-10 text-base font-semibold leading-5 text-gray-500 text-ellipsis">
           {description}
         </p>
       </div>
