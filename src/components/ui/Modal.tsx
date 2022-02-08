0e0d265bee4f2fa12b0945769933eaf31df0e41e
Kosuke2000 <youuchiharu@gmail.com>
2022-02-08 22:19:33 +0900

    remove dark mode

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -37,7 +37,7 @@ export const Modal: VFC<ModalProps> = ({ title, code, close }) => {
       className={`flex overflow-y-hidden fixed top-0 left-0 z-10 justify-center items-center w-screen h-screen bg-slate-200/75  ${u}`}
     >
       <div
-        className={`flex flex-col justify-center items-center py-8 w-4/5 h-4/5 bg-white dark:bg-slate-900 rounded shadow  animate-fade-in  `}
+        className={`flex flex-col justify-center items-center py-8 w-4/5 h-4/5 bg-white  rounded shadow  animate-fade-in  `}
       >
         <h2 className="pb-6 text-2xl font-bold">{title}</h2>
         <SyntaxHighlighter
@@ -51,14 +51,14 @@ export const Modal: VFC<ModalProps> = ({ title, code, close }) => {
         <div className="flex justify-between w-full max-w-md">
           <button
             onClick={onCancel}
-            className="py-2 px-4 font-semibold text-blue-700 hover:text-white dark:text-white bg-transparent hover:bg-blue-500 dark:hover:bg-slate-800 rounded border border-blue-500 hover:border-transparent dark:border-yellow-300 "
+            className="py-2 px-4 font-semibold text-blue-700 hover:text-white bg-transparent hover:bg-blue-500 rounded border border-blue-500 hover:border-transparent"
           >
             Cancel
           </button>
           {copied ? <Tooltip /> : <></>}
 
           <CopyToClipboard text={code} onCopy={onCopy}>
-            <button className="py-2 px-6 font-bold text-white dark:text-slate-700 bg-blue-500 hover:bg-blue-700 dark:bg-yellow-300 dark:hover:bg-yellow-500 rounded">
+            <button className="py-2 px-6 font-bold text-white bg-blue-500 hover:bg-blue-700 rounded">
               Copy
             </button>
           </CopyToClipboard>
